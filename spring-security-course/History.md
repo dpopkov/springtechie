375cde909c0c26808bef26747aa25aecfa1a7a22
Denis Popkov <pkvdenis@gmail.com>
2021-03-02 21:03:24 +0300

    spring-security-course: build the simplest API: add simple model class and REST controller.

---
+++ spring-security-course/History.md
@@ -0,0 +1,4 @@
+History of implementation
+-------------------------
+
+1. Build the simplest API: add simple model class and REST controller.
---
+++ spring-security-course/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = List.of(
+            new Student(1, "Bobby"),
+            new Student(2, "Alice"),
+            new Student(3, "Timmy")
+    );
+
+    @GetMapping("{studentId}")
+    public Student getStudent(@PathVariable Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> student.getStudentId().equals(studentId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist"));
+    }
+}
