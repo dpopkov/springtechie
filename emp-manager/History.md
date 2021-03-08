25624c3403da1b1f9b2d26d25988e4ff13dc8ca5
Denis Popkov <pkvdenis@gmail.com>
2021-03-08 19:47:00 +0300

    emp-manager: implement loading bootstrap data from JSON file, add images.

--- emp-manager/History.md
+++ emp-manager/History.md
@@ -7,3 +7,4 @@ History of Implementation
 4. Add bootstrap data.
 5. Add REST Controller to expose API.
 6. Add CorsFilter bean to configure Cross-Origin Resource Sharing (CORS) for Front-End.
+7. Implement loading bootstrap data from JSON file.
--- emp-manager/src/main/java/com/example/empmanager/bootstrap/DataLoader.java
+++ emp-manager/src/main/java/com/example/empmanager/bootstrap/DataLoader.java
@@ -2,13 +2,21 @@ package com.example.empmanager.bootstrap;
 
 import com.example.empmanager.model.Employee;
 import com.example.empmanager.service.EmployeeService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 @Slf4j
 @Component
 public class DataLoader implements CommandLineRunner {
+
+    private static final String JSON_DATA_PATH = "/json/data.json";
 
     private final EmployeeService service;
 
@@ -19,15 +27,28 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (service.count() == 0) {
-            loadEmployees();
+            loadData();
         }
     }
 
-    private void loadEmployees() {
-        Employee e1 = Employee.builder().name("Bobby").phone("123-4567").jobTitle("Tester").build();
-        Employee e2 = Employee.builder().name("Alice").phone("456-4567").jobTitle("Dev").build();
-        service.addEmployee(e1);
-        service.addEmployee(e2);
+    private void loadData() {
+        var employees = loadFromJson();
+        for (Employee emp : employees) {
+            service.addEmployee(emp);
+        }
         log.info("Loaded {} Employees.", service.count());
+    }
+
+    private List<Employee> loadFromJson() {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        TypeReference<List<Employee>> typeReference = new TypeReference<>() {
+        };
+        List<Employee> employees = List.of();
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream(JSON_DATA_PATH)) {
+            employees = jsonMapper.readValue(inputStream, typeReference);
+        } catch (IOException e) {
+            log.error("Error reading employees from {}", JSON_DATA_PATH, e);
+        }
+        return employees;
     }
 }
